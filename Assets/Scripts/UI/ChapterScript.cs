97442a3117b89915e2e51b45badf8a69e7316061
Sam <samanthavanzandwijk@gmail.com>
2023-05-12 14:05:26 +0200

    chapter text done

---
+++ Assets/Scripts/UI/ChapterScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ChapterScript : MonoBehaviour
+{
+    [SerializeField] int seconds;
+    [SerializeField] TextMeshProUGUI tmpro;
+    [SerializeField] string textToWrite;
+
+    GameObject chapterText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chapterText = GameObject.Find("ChapterText");
+        StartCoroutine(Timer());
+        tmpro.text = textToWrite;
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(seconds);
+        chapterText.GetComponent<Animator>().SetTrigger("Fade"); 
+    }
+
+    public void DisableText()
+    {
+        chapterText.SetActive(false);
+    }
+
+}
